d565f28bc653fe30e5a372e9235b8cd2b8eb5c93
Ville Heikkilä <mail@villeheikkila.com>
2019-07-05 04:05:10 +0300

    Add skeleton for the backend. Add skeleton for Prisma. Implement GraphQL mutation for adding new users to the database

--- tastice-backend/src/index.ts
+++ tastice-backend/src/index.ts
@@ -1,1 +1,34 @@
-console.log('Hello frontend!');
+import { prisma } from '../generated/prisma-client'
+import datamodelInfo from '../generated/nexus-prisma'
+import * as path from 'path'
+import { stringArg, idArg } from 'nexus'
+import { prismaObjectType, makePrismaSchema } from 'nexus-prisma'
+import { GraphQLServer } from 'graphql-yoga'
+
+
+const Mutation = prismaObjectType({
+    name: 'Mutation',
+    definition(t) {
+        t.prismaFields(['createUser'])
+    },
+})
+
+const schema = makePrismaSchema({
+    types: [Mutation],
+
+    prisma: {
+        datamodelInfo,
+        client: prisma,
+    },
+
+    outputs: {
+        schema: path.join(__dirname, './generated/schema.graphql'),
+        typegen: path.join(__dirname, './generated/nexus.ts'),
+    },
+})
+
+const server = new GraphQLServer({
+    schema,
+    context: { prisma },
+})
+server.start(() => console.log('Server is running on http://localhost:4000'))
--- timelog.md
+++ timelog.md
@@ -3,3 +3,4 @@
 | Date  | Time (hours) | Tasks                                                                          |           
 | ------| -------------| -------------------------------------------------------------------------------|
 | 4.7. | 2            | Initialize the project, pick a name, decide the key technologies (React/TypeScrip/Node/GraphQL/Postgresql), write the initial readme, add React/TypeScript skeleton and webpack configs |
+| 5.7. | 3,5            | Mess around with webpack on the frontend (no commits), create skeleton for backend, add initial Prisma integration, implement GraphQL API for adding users |
