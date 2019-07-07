893c3005cc68aadf216c8ac7bb4becbc2960eba6
Ville Heikkilä <mail@villeheikkila.com>
2019-07-08 01:24:44 +0300

    Add skeleton for sign in page

---
+++ tastice-frontend/src/components/SignUp/index.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+export const SignUp = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const submit = async (event: React.FormEvent<HTMLFormElement>
+    ): Promise<void> => {
+
+        event.preventDefault()
+
+        setEmail("")
+        setPassword("")
+    }
+
+    return (
+        <div>
+            <form onSubmit={submit}>
+                <div>
+                    <p>Email</p>
+                    <input
+                        value={email}
+                        onChange={({ target }) => setEmail(target.value)}
+                    />
+                </div>
+                <div>
+                    <p>Password</p>
+                    <input
+                        value={password}
+                        onChange={({ target }) => setPassword(target.value)}
+                    />
+                </div>
+                <button type='submit'>Add product!</button>
+            </form>
+        </div>
+    )
+}
