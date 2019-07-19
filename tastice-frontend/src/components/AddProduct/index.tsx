8d121034c78568b12b65ce285f3b8d5a7c65905b
Ville Heikkilä <mail@villeheikkila.com>
2019-07-19 12:55:08 +0300

    Move MaterialSelect component from under /AddProduct to /components because it'll be used later on CreatCheckIn component

--- tastice-frontend/src/components/AddProduct/index.tsx
+++ tastice-frontend/src/components/AddProduct/index.tsx
@@ -8,7 +8,7 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
-import { MaterialSelect } from "./MaterialSelect";
+import { MaterialSelect } from "../MaterialSelect";
 import { OptionType } from "../../types";
 import useReactRouter from "use-react-router";
 
