69cdbe05843a55b81fc27bc678efc1a97b76413d
Ville Heikkilä <mail@villeheikkila.com>
2019-07-30 19:01:22 +0300

    Split ProductCardMenu from ProductCard

---
+++ tastice-frontend/src/components/ProductCard/ProductCardMenu.tsx
@@ -0,0 +1,74 @@
+import { useMutation } from '@apollo/react-hooks';
+import { Menu, MenuItem } from '@material-ui/core';
+import { bindMenu } from 'material-ui-popup-state/hooks';
+import React, { useState } from 'react';
+import useReactRouter from 'use-react-router';
+import { ALL_CHECKINS, ALL_PRODUCTS, DELETE_PRODUCT } from '../../queries';
+import { errorHandler, notificationHandler } from '../../utils';
+import { ConfirmationDialog } from '../ConfirmationDialog';
+
+export interface ProductCardMenuProps {
+    setShowEditProduct: any;
+    menuState: any;
+    id: string;
+    name: string;
+}
+
+export const ProductCardMenu = ({ id, name, menuState, setShowEditProduct }: ProductCardMenuProps): JSX.Element => {
+    const [visible, setVisible] = useState(false);
+    const { history } = useReactRouter();
+
+    const [deleteProduct] = useMutation(DELETE_PRODUCT, {
+        onError: errorHandler,
+        refetchQueries: [{ query: ALL_PRODUCTS }, { query: ALL_CHECKINS }],
+    });
+
+    const handleDeleteProduct = async (): Promise<void> => {
+        setVisible(false);
+        const result = await deleteProduct({
+            variables: { id },
+        });
+
+        if (result) {
+            notificationHandler({
+                message: `Product ${result.data.deleteProduct.name} succesfully deleted`,
+                variant: 'success',
+            });
+            history.push(`/activity`);
+        }
+    };
+
+    return (
+        <>
+            <Menu {...bindMenu(menuState)}>
+                <MenuItem
+                    onClick={(): void => {
+                        menuState.close();
+                        setShowEditProduct(true);
+                    }}
+                >
+                    Edit Product
+                </MenuItem>
+                <MenuItem
+                    onClick={(): void => {
+                        menuState.close();
+                        setVisible(true);
+                    }}
+                >
+                    Remove Product
+                </MenuItem>
+            </Menu>
+
+            <ConfirmationDialog
+                visible={visible}
+                setVisible={setVisible}
+                description={'HEEII'}
+                title={'Warning!'}
+                content={`Are you sure you want to remove ${name}`}
+                onAccept={handleDeleteProduct}
+                declineButton={'Cancel'}
+                acceptButton={'Yes'}
+            />
+        </>
+    );
+};
--- tastice-frontend/src/components/ProductCard/index.tsx
+++ tastice-frontend/src/components/ProductCard/index.tsx
@@ -1,14 +1,11 @@
-import { useMutation } from '@apollo/react-hooks';
-import { Avatar, CardActionArea, Chip, Grid, Link, makeStyles, Menu, MenuItem, Typography } from '@material-ui/core';
+import { Avatar, CardActionArea, Chip, Grid, Link, makeStyles, Typography } from '@material-ui/core';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-import { bindMenu, bindTrigger, usePopupState } from 'material-ui-popup-state/hooks';
+import { bindTrigger, usePopupState } from 'material-ui-popup-state/hooks';
 import React, { useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import useReactRouter from 'use-react-router';
 import lipton from '../../images/lipton.jpg';
-import { ALL_PRODUCTS, DELETE_PRODUCT } from '../../queries';
-import { errorHandler, notificationHandler } from '../../utils';
-import { ConfirmationDialog } from '../ConfirmationDialog';
+import { ProductCardMenu } from './ProductCardMenu';
 import { UpdateProduct } from './UpdateProduct';
 
 const useStyles = makeStyles(theme => ({
@@ -35,65 +32,11 @@ interface ProductCardProps {
 
 export const ProductCard = ({ product, showMenu }: ProductCardProps): JSX.Element => {
     const classes = useStyles();
-    const [visible, setVisible] = useState(false);
     const [showEditProduct, setShowEditProduct] = useState();
     const { history } = useReactRouter();
     const menuState = usePopupState({ variant: 'popover', popupId: 'CheckInMenu' });
-    const [deleteProduct] = useMutation(DELETE_PRODUCT, {
-        onError: errorHandler,
-        refetchQueries: [{ query: ALL_PRODUCTS }],
-    });
 
     const { id, name, company, category, subCategory } = product;
-
-    const handleDeleteProduct = async (): Promise<void> => {
-        setVisible(false);
-        const result = await deleteProduct({
-            variables: { id: product.id },
-        });
-
-        if (result) {
-            notificationHandler({
-                message: `Product ${result.data.deleteProduct.name} succesfully deleted`,
-                variant: 'success',
-            });
-            history.push(`/activity`);
-        }
-    };
-
-    const menu = (
-        <div>
-            <Menu {...bindMenu(menuState)}>
-                <MenuItem
-                    onClick={(): void => {
-                        menuState.close();
-                        setShowEditProduct(true);
-                    }}
-                >
-                    Edit Product
-                </MenuItem>
-                <MenuItem
-                    onClick={(): void => {
-                        menuState.close();
-                        setVisible(true);
-                    }}
-                >
-                    Remove Product
-                </MenuItem>
-            </Menu>
-
-            <ConfirmationDialog
-                visible={visible}
-                setVisible={setVisible}
-                description={'HEEII'}
-                title={'Warning!'}
-                content={`Are you sure you want to remove ${product.name}`}
-                onAccept={handleDeleteProduct}
-                declineButton={'Cancel'}
-                acceptButton={'Yes'}
-            />
-        </div>
-    );
 
     return (
         <>
@@ -142,7 +85,12 @@ export const ProductCard = ({ product, showMenu }: ProductCardProps): JSX.Elemen
                         {showMenu && (
                             <Grid>
                                 <MoreVertIcon {...bindTrigger(menuState)} />
-                                {menu}
+                                <ProductCardMenu
+                                    id={product.id}
+                                    name={product.name}
+                                    menuState={menuState}
+                                    setShowEditProduct={setShowEditProduct}
+                                />
                             </Grid>
                         )}
                     </Grid>
