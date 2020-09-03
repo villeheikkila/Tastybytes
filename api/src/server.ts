82a8b941e7eb72ca882b4212874fb9df18d18b32
Ville Heikkilä <mail@villeheikkila.com>
2020-09-03 23:59:39 +0300

    build: add docker-compose

--- api/src/server.ts
+++ api/src/server.ts
@@ -6,11 +6,16 @@ import { AccountResolver } from "./resolvers/AccountsResolver";
 import { typeOrmConfig } from "./config";
 
 (async () => {
-  await createConnection(typeOrmConfig);
-  console.log("Connected to the PostgreSQL database");
-  const schema = await buildSchema({ resolvers: [AccountResolver] });
-  const server = new ApolloServer({ schema });
+  try {
+    await createConnection(typeOrmConfig);
+    console.log("Connected to the PostgreSQL database");
+    const schema = await buildSchema({ resolvers: [AccountResolver] });
+    const server = new ApolloServer({ schema });
 
-  await server.listen(4000);
-  console.log(`Server has started on ${4000}`);
+    await server.listen(4000);
+    console.log(`Server has started on ${4000}`);
+  } catch (error) {
+    console.log("USER: ", process.env.POSTGRES_USER);
+    console.error(error);
+  }
 })();
