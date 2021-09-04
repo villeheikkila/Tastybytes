004ee34170e313f62d43517dab9a36c5f8cf8ac6
Ville Heikkilä <mail@villeheikkila.com>
2021-09-04 15:44:18 +0300

    chore(server): drop redis

--- @app/server/src/middleware/installSession.ts
+++ @app/server/src/middleware/installSession.ts
@@ -1,13 +1,10 @@
 import ConnectPgSimple from "connect-pg-simple";
-import ConnectRedis from "connect-redis";
 import { Express, RequestHandler } from "express";
 import session from "express-session";
-import * as redis from "redis";
 
 import { getWebsocketMiddlewares } from "../app";
 import { getRootPgPool } from "./installDatabasePools";
 
-const RedisStore = ConnectRedis(session);
 const PgStore = ConnectPgSimple(session);
 
 const MILLISECOND = 1;
@@ -27,24 +24,7 @@ const MAXIMUM_SESSION_DURATION_IN_MILLISECONDS =
 export default (app: Express) => {
   const rootPgPool = getRootPgPool(app);
 
-  const store = process.env.REDIS_URL
-    ? /*
-       * Using redis for session storage means the session can be shared across
-       * multiple Node.js instances (and survives a server restart), see:
-       *
-       * https://medium.com/mtholla/managing-node-js-express-sessions-with-redis-94cd099d6f2f
-       */
-      new RedisStore({
-        client: redis.createClient({
-          url: process.env.REDIS_URL,
-        }),
-      })
-    : /*
-       * Using PostgreSQL for session storage is easy to set up, but increases
-       * the load on your database. We recommend that you graduate to using
-       * redis for session storage when you're ready.
-       */
-      new PgStore({
+  const store = new PgStore({
         /*
          * Note even though "connect-pg-simple" lists "pg@7.x" as a dependency,
          * it doesn't `require("pg")` if we pass it a pool. It's usage of the pg
