fa5bf63bfa59d20173a35fe60f5f9e7f3b31d1fa
Ville Heikkilä <mail@villeheikkila.com>
2021-09-08 00:16:56 +0300

    config: disable tests in watch mode, enable logs

--- @app/server/src/middleware/installLogging.ts
+++ @app/server/src/middleware/installLogging.ts
@@ -5,8 +5,7 @@ const isDev = process.env.NODE_ENV === "development";
 
 export default (app: Express) => {
   if (isDev) {
-    // To enable logging on development, uncomment the next line:
-    // app.use(morgan("tiny"));
+    app.use(morgan("tiny"));
   } else {
     app.use(morgan(isDev ? "tiny" : "combined"));
   }
