5031247c8a297e0b089364bd82b2cca725e83e39
Ville Heikkilä <mail@villeheikkila.com>
2021-10-22 23:33:46 +0300

    feat(friends): more work on making the friend request / user search flow usable

--- @pwa/client/src/pages/users/[username]/friends.tsx
+++ @pwa/client/src/pages/users/[username]/friends.tsx
@@ -1,17 +1,7 @@
-import { faCheck, faMinus, faPlus, faUserClock } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon, FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
-import { Nullable, paths } from "@pwa/common";
-import { Card, Layout, SharedLayout } from "@pwa/components";
-import { styled } from "@pwa/components";
+import { paths, useFriendStatus } from "@pwa/common";
+import { Card, FriendStatusIcon, Layout, SharedLayout, styled } from "@pwa/components";
 import {
-  CurrentUserFriendsQuery,
-  FriendsByUsernameQuery,
-  FriendStatus,
-  useAcceptFriendRequestMutation,
-  useCurrentUserFriendsQuery,
-  useDeleteFriendMutation,
-  useFriendsByUsernameQuery,
-  useSendFriendRequestMutation,
+  FriendsByUsernameQuery, useFriendsByUsernameQuery
 } from "@pwa/graphql";
 import { NextPage } from "next";
 import Link from "next/link";
@@ -24,7 +14,7 @@ const parseSlug = z.string();
 const FriendsPage: NextPage = () => {
   const router = useRouter();
   const username = parseSlug.parse(router.query.username);
-  
+
   const query = useFriendsByUsernameQuery({variables: {username}});
   const data = query.data?.userByUsername;
 
@@ -42,42 +32,10 @@ interface FriendsPageInnerProps {
   data: NonNullable<FriendsByUsernameQuery["userByUsername"]>;
 }
 
-const mutationOptions = {
-  refetchQueries: ["CurrentUserFriends"],
-};
-
 const FriendsPageInner: FC<FriendsPageInnerProps> = ({ data }) => {
-  const [sendFriendRequest] = useSendFriendRequestMutation(mutationOptions);
-  const [acceptFriendRequest] = useAcceptFriendRequestMutation(mutationOptions);
-  const [removeFriend] = useDeleteFriendMutation(mutationOptions);
-
-  const handleFriendStatusChange = async (
-    userId: string,
-    status: Nullable<FriendStatus>,
-    isSender: boolean
-  ) => {
-    try {
-      switch (status) {
-        case FriendStatus.Accepted:
-          await removeFriend({ variables: { userId } });
-          break;
-        case FriendStatus.Pending: {
-          if (isSender) {
-            await removeFriend({ variables: { userId } });
-          } else {
-          await acceptFriendRequest({ variables: { userId } });
-          }
-          break;
-        }
-        default:
-          await sendFriendRequest({ variables: { userId } });
-          break;
-      }
-    } catch (error) {
-      console.error(error);
-    }
-    console.log("userId: ", userId);
-  };
+  const { changeFriendStatus } = useFriendStatus({
+    refetchQueries: ["FriendsByUsername"],
+  });
 
   return (
     <Layout.Root>
@@ -92,40 +50,21 @@ const FriendsPageInner: FC<FriendsPageInnerProps> = ({ data }) => {
                 <h2>{friend.username}</h2>
               </Link>
               <FriendStatusIcon
-                status={friend.currentUserStatus}
+                friendStatus={friend}
                 size="2x"
                 onClick={() =>
-                  handleFriendStatusChange(friend.id, friend.currentUserStatus, true)
+                  changeFriendStatus(friend.id, friend)
                 }
               />
             </Flex>
           </Card.Wrapper>
         ))}
-        
+
       </Card.Container>
     </Layout.Root>
   );
 };
 
-const FriendStatusIcon = ({
-  status,
-  ...props
-}: Omit<FontAwesomeIconProps, "icon" | "color"> & {
-  status: Nullable<FriendStatus>;
-}): JSX.Element => {
-  switch (status) {
-    case FriendStatus.Accepted:
-      return <FontAwesomeIcon icon={faMinus} color="red" {...props} />;
-    case FriendStatus.Pending:
-      return (
-        <FontAwesomeIcon icon={faUserClock} color="green" {...props} />
-      );
-   default:
-      return (
-        <FontAwesomeIcon icon={faPlus} color="green" {...props} />
-      );
-  }
-};
 const Flex = styled("div", {
   display: "flex",
   justifyContent: "space-between",
