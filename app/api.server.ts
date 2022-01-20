dd008a28eaa6061fdc4dc71e673df4dd8b16dc8b
Ville Heikkilä <mail@villeheikkila.com>
2022-01-20 22:49:42 +0200

    Fix and rename SDK

--- app/api.server.ts
+++ app/api.server.ts
@@ -1,8 +1,10 @@
-import { getSdk } from "../generated/client";
+import * as SDK from "../generated/sdk";
 import { GraphQLClient } from "graphql-request";
 
 const client = new GraphQLClient("http://localhost:3333/graphql");
 
 export const sdk = () => {
-    return getSdk(client); 
+    return SDK.getSdk(client); 
 }
+
+export default SDK
