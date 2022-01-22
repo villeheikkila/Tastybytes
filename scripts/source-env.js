38ec764af2b4a33dff22ddc3328ddcdd05d10e3d
Ville Heikkilä <mail@villeheikkila.com>
2022-01-22 14:28:05 +0200

    Extend env sourcing script to support different environment for prod

--- scripts/source-env.js
+++ scripts/source-env.js
@@ -1,1 +1,7 @@
-require("dotenv").config({ path: `${__dirname}/../.env` });
+if (process.env.NODE_ENV === "production") {
+  console.log("Running against production");
+  require("dotenv").config({ path: `${__dirname}/../.env.prod` });
+} else {
+  console.log("Running against development");
+  require("dotenv").config({ path: `${__dirname}/../.env` });
+}
