539ec412c9f3fd8d424407a15a7cebf6e56e96dd
Ville Heikkilä <mail@villeheikkila.com>
2022-01-23 17:58:23 +0200

    Consistently use const

--- app/routes/products/$productId.tsx
+++ app/routes/products/$productId.tsx
@@ -4,7 +4,6 @@ import { Card } from "~/components/card";
 import { Layout } from "~/components/layout";
 import { Stars } from "~/components/stars";
 import { Typography } from "~/components/typography";
-import { styled } from "~/stitches.config";
 import { paths } from "~/utils/paths";
 
 export const loader: LoaderFunction = async ({
@@ -12,11 +11,10 @@ export const loader: LoaderFunction = async ({
   params,
 }): Promise<SDK.GetProductByIdQuery> => {
   if (!params.productId) {
-    throw new Response("What a joke! Not found.", { status: 404 });
+    throw new Response("Not found.", { status: 404 });
   }
 
   const productId = parseInt(params.productId, 10);
-  console.log("productId: ", productId);
   const companies = await sdk().getProductById({ productId });
   return companies;
 };
@@ -28,20 +26,20 @@ export default function Index() {
     <Layout.Root>
       <Layout.Header>
         <Typography.H1>
-          {product?.brand?.company?.name} {product?.brand?.name} {product.name}
+          {product?.brand?.company?.name} {product?.brand?.name} {product?.name}
         </Typography.H1>
       </Layout.Header>
 
       <Card.Container>
-        {product.checkIns.nodes.map(({ id, author, rating }) => (
+        {product?.checkIns.nodes.map(({ id, author, rating }) => (
           <Card.Wrapper key={id}>
             <p>
               <b>{author.username}</b> has tasted{" "}
               <Link
                 to={paths.products(id)}
-              >{`${product?.brand?.name} - ${product.name}`}</Link>{" "}
+              >{`${product?.brand?.name} - ${product?.name}`}</Link>{" "}
               by{" "}
-              <Link to={`/company/${product?.brand?.company?.name}`}>
+              <Link to={paths.company(product?.brand?.company.id)}>
                 {product?.brand?.company?.name}
               </Link>
             </p>
@@ -52,5 +50,3 @@ export default function Index() {
     </Layout.Root>
   );
 }
-
-const H1 = styled("h1", { fontWeight: "bold", color: "$red" });
