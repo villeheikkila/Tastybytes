1beb81218ef54a1f2f281345d6c945ea8d52230e
Ville Heikkilä <mail@villeheikkila.com>
2022-04-16 14:00:07 +0300

    Replace nextjs with remix based implementation

--- pages/api/auth.ts
+++
@@ -1,9 +0,0 @@
-/**
- * NOTE: this file is only needed if you're doing SSR (getServerSideProps)!
- */
-import { NextApiRequest, NextApiResponse } from 'next'
-import { supabase } from '../../utils/initSupabase'
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  supabase.auth.api.setAuthCookie(req, res)
-}
