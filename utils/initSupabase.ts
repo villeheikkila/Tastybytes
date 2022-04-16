b0f166893f0bfe59e1fb71bdab6d3a0fb14b898e
Ville Heikkilä <mail@villeheikkila.com>
2022-04-16 14:00:07 +0300

    Replace nextjs with remix based implementation

--- utils/initSupabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export const supabase = createClient(
-  String(process.env.NEXT_PUBLIC_SUPABASE_URL),
-  String(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
-)
