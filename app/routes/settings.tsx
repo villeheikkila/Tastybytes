9b5d8612214b7aa7341fe85b43ead8a14a66ad31
Ville Heikkilä <mail@villeheikkila.com>
2022-04-24 20:29:01 +0300

    Work on avatar upload

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -1,7 +1,82 @@
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
+import {
+  unstable_createFileUploadHandler,
+  unstable_parseMultipartFormData,
+} from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import { authenticator, supabaseStrategy } from "~/auth.server";
+import { supabaseClient } from "~/supabase";
+
+export const action: ActionFunction = async ({ request }) => {
+  const session = await supabaseStrategy.checkSession(request);
+  if (!session) throw Error("User is not logged in!");
+  supabaseClient.auth.setAuth(session.access_token);
+
+  const uploadHandler = unstable_createFileUploadHandler({
+    maxFileSize: 5_000_000,
+    file: ({ filename }) => filename,
+  });
+  const formData = await unstable_parseMultipartFormData(
+    request,
+    uploadHandler
+  );
+
+  const file = formData.get("avatar");
+  console.log("file: ", file);
+
+  if (file && session?.user?.id) {
+    const { data, error } = await supabaseClient.storage
+      .from("avatars")
+      .upload(`${session?.user?.id}/${(file as unknown as any).name}`, file, {
+        contentType: (file as unknown as any).type,
+        cacheControl: "3600",
+        upsert: true,
+      });
+
+    if (data?.Key) {
+      let { error } = await supabaseClient.from("profiles").upsert({
+        id: session?.user?.id,
+        avatar_url: data.Key,
+      });
+
+      console.log("error: ", error);
+    }
+  }
+
+  return null;
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const session = await authenticator.isAuthenticated(request);
+  const { data: user } = await supabaseClient
+    .from("profiles")
+    .select("*")
+    .eq("id", session?.user?.id)
+    .single();
+
+  return { user };
+};
 export default function Index() {
+  const { user } = useLoaderData();
+
   return (
     <>
       <h1>Settings</h1>
+
+      <Form method="post" encType="multipart/form-data">
+        <label htmlFor="avatar-input">Avatar</label>
+        <input id="avatar-input" type="file" name="avatar" />
+        <button>Upload</button>
+        {user && (
+          <img
+            src={
+              "https://iykihowuxxkqxobggkuk.supabase.co/storage/v1/object/public/" +
+              user.avatar_url
+            }
+            alt="avatar"
+          />
+        )}
+      </Form>
     </>
   );
 }
