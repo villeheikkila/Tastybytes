1be0cab85487511343a9cd9b29b11bbd32cc92de
Ville Heikkilä <mail@villeheikkila.com>
2022-04-24 16:37:00 +0300

    Add sign up page

--- app/routes/sign-up.tsx
+++ app/routes/sign-up.tsx
@@ -1,7 +1,162 @@
-export default function Index() {
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Form, Link, useActionData } from "@remix-run/react";
+import { getFormData } from "remix-params-helper";
+import { z } from "zod";
+import { supabaseStrategy } from "~/auth.server";
+import { styled } from "~/stitches.config";
+import { supabaseClient } from "~/supabase";
+import { paths } from "~/utils";
+
+export const action: ActionFunction = async ({ request }) => {
+  const {
+    success,
+    data: signUpForm,
+    errors,
+  } = await getFormData(
+    request,
+    z
+      .object({
+        email: z.string().email().optional(),
+        password: z.string(),
+        confirm: z.string(),
+      })
+      .refine((data) => data.password === data.confirm, {
+        message: "Passwords don't match",
+        path: ["confirm"],
+      })
+  );
+
+  if (success) {
+    const { session, error } = await supabaseClient.auth.signUp({
+      email: signUpForm.email,
+      password: signUpForm.password,
+    });
+
+    console.log("s ", session);
+
+    return json({ errors: error });
+  }
+
+  return json({ errors });
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  await supabaseStrategy.checkSession(request, {
+    successRedirect: "/",
+  });
+
+  return null;
+};
+
+export default function Screen() {
+  const ac = useActionData();
+  console.log("ac: ", ac);
   return (
-    <>
-      <h1>Sign Up</h1>
-    </>
+    <Wrapper data-light="">
+      <Header>
+        <img
+          color="white"
+          alt="icon of tasted"
+          src="/favicon.svg"
+          height={48}
+          width={48}
+        />
+        <H1>Welcome to Tasted</H1>
+        <p>
+          Already have an account? <Link to={paths.login}>Login!</Link>
+        </p>
+      </Header>
+      <StyledForm method="post">
+        <Input
+          autoComplete="email"
+          aria-label="email"
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Email"
+        />
+
+        <Input
+          type="password"
+          placeholder="Password"
+          aria-label="password"
+          autoComplete="new-password"
+          name="password"
+          id="password"
+        />
+        <Input
+          type="password"
+          placeholder="Confirm password"
+          aria-label="password"
+          autoComplete="new-password"
+          name="confirm"
+          id="confirm-password"
+        />
+        <Button>Sign Up</Button>
+      </StyledForm>
+    </Wrapper>
   );
 }
+
+const H1 = styled("h1", { fontSize: "28px" });
+
+export const ErrorText = styled("em", { color: "$red" });
+
+export const Button = styled("button", {
+  backgroundColor: "#0099ff",
+  borderRadius: "10px",
+  boxShadow: "#000000 0px 1px 2px 0px",
+  color: "#ffffff",
+  fontSize: "16px",
+  fontWeight: 500,
+  lineHeight: "15px",
+  padding: "0px 16px",
+  textAlign: "center",
+  height: "40px",
+  border: "none",
+  variants: {
+    variant: {
+      warning: {
+        backgroundColor: "$red",
+      },
+    },
+  },
+});
+
+const Wrapper = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  gap: "16px",
+  width: "330px",
+});
+
+export const Input = styled("input", {
+  backgroundColor: "#333333",
+  borderRadius: "10px",
+  color: "#bababa",
+  display: "inline-block",
+  padding: "0px 16px",
+  fontSize: "16px",
+  height: "40px",
+  border: "none",
+  "&:focus": { outline: "1px solid $blue" },
+  transition: "outline 0.4s ease 0s, color 0.2s ease 0s",
+  "&[aria-invalid='true']": {
+    outline: "1px solid red",
+  },
+});
+
+const Header = styled("header", {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "10px",
+});
+
+const StyledForm = styled(Form, {
+  display: "flex",
+  flexDirection: "column",
+  gap: "10px",
+});
--- app/utils/index.ts
+++ app/utils/index.ts
@@ -5,6 +5,7 @@ export const paths = {
     settings: "/settings",
     logout: "/logout",
     signUp: "/sign-up",
+    login: "/login",
     product: (product: Product) => {
         return `/products/${product.subcategories.categories.name}/${product.subcategories.name}/${product.sub_brands.brands.companies.name}/${product.sub_brands.brands.name}/${product.sub_brands.name}/${product.name}`;
       }
