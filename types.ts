56dbf7d3c287e7cdd0c1b02c997133df0ffbfe0a
Ville Heikkilä <mail@villeheikkila.com>
2022-09-10 13:04:27 +0900

    Add types file

---
+++ types.ts
@@ -0,0 +1,52 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[];
+
+export interface Database {
+  public: {
+    Tables: {
+      categories: {
+        Row: {
+          id: number;
+          name: string;
+        };
+        Insert: {
+          id?: number;
+          name: string;
+        };
+        Update: {
+          id?: number;
+          name?: string;
+        };
+      };
+      companies: {
+        Row: {
+          id: number;
+          name: string | null;
+        };
+        Insert: {
+          id?: never;
+          name?: string | null;
+        };
+        Update: {
+          id?: never;
+          name?: string | null;
+        };
+      };
+    };
+    Views: {
+      [_ in never]: never;
+    };
+    Functions: {
+      [_ in never]: never;
+    };
+    Enums: {
+      [_ in never]: never;
+    };
+  };
+}
+
