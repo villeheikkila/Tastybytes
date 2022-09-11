b475ce3864c3c0920ad035d2962418db24f7b19d
Ville Heikkilä <ville.heikkila@reaktor.fi>
2022-09-12 00:11:00 +0900

    Set up Konsta as a component library

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,11 +1,16 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+const konstaConfig = require("konsta/config");
+
+module.exports = konstaConfig({
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "media", // or 'class'
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-};
+});
