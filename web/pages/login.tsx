684be4a3537420e7e3cc462d97dd462c36ecee00
Ville Heikkilä <ville.heikkila@reaktor.fi>
2022-09-12 01:01:30 +0900

    Add some initial styling to profile and login page, navigate on login / logout

---
+++ web/pages/login.tsx
@@ -0,0 +1,36 @@
+import { supabaseClient } from "@supabase/auth-helpers-nextjs";
+import { useUser } from "@supabase/auth-helpers-react";
+import { Auth } from "@supabase/ui";
+import { Navbar, Page } from "konsta/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+const LoginPage = () => {
+  const { user } = useUser();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user?.id) {
+      router.push("/");
+    }
+  }, [user?.id, router]);
+
+  return (
+    <Page>
+      <Navbar title="Login" />
+
+      <main className="grid h-screen justify-center">
+        <div>
+          <Auth
+            supabaseClient={supabaseClient}
+            providers={[]}
+            socialLayout="horizontal"
+            socialButtonSize="xlarge"
+          />
+        </div>
+      </main>
+    </Page>
+  );
+};
+
+export default LoginPage;
