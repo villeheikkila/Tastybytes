ceb091abff4d3f271274b989f6ffffac9fb11da7
Ville Heikkilä <mail@villeheikkila.com>
2022-09-11 23:14:31 +0900

    Add simple auth based fetch on a protected route

--- web/pages/profile.tsx
+++ web/pages/profile.tsx
@@ -1,30 +1,30 @@
 import {
-    User,
-    withPageAuth,
-    supabaseServerClient
-  } from '@supabase/auth-helpers-nextjs';
-  
-  export default function ProfilePage({
-    user,
-    data
-  }: {
-    user: User,
-    data: any
-  }) {
-    return (
-      <>
-        <div>Protected content for {user.email}</div>
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-        <pre>{JSON.stringify(user, null, 2)}</pre>
-      </>
-    );
-  }
-  
-  export const getServerSideProps = withPageAuth({
-    redirectTo: '/',
-    async getServerSideProps(ctx) {
-      // Run queries with RLS on the server
-      const { data } = await supabaseServerClient(ctx).from('profiles').select('*');
-      return { props: { data } };
-    }
-  });
+  User,
+  withPageAuth,
+  supabaseServerClient,
+  getUser,
+} from "@supabase/auth-helpers-nextjs";
+import Title from "@supabase/ui/dist/cjs/components/Typography/Title";
+
+import {Database} from "../generated/DatabaseDefinitions"
+
+export default function ProfilePage({ user, data }: { user: User; data: Database["public"]["Tables"]["profiles"]["Row"]}) {
+    console.log('data: ', data);
+  return (
+    <>
+      <div><Title>{data.username}</Title></div>
+    </>
+  );
+}
+
+export const getServerSideProps = withPageAuth({
+  redirectTo: "/",
+  async getServerSideProps(ctx) {
+    const { user } = await getUser(ctx);
+    const { data } = await supabaseServerClient(ctx)
+      .from("profiles")
+      .select("*")
+      .match({ id: user.id }).limit(1);
+    return { props: { data: data?.[0] } };
+  },
+});
