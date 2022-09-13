236234a0f1f0e8d12bfd9925ed24c5a6bf5e42f9
Ville Heikkilä <mail@villeheikkila.com>
2022-09-14 01:42:39 +0900

    Use supabase client csv function instead of the home grown one

--- web/utils/export-check-ins.ts
+++ web/utils/export-check-ins.ts
@@ -1,34 +1,22 @@
 import { supabaseClient } from "@supabase/auth-helpers-nextjs";
 
-function toCSV(data: object[]): string {
-    const header = Object.keys(data[0]).map(String) 
-    const dataToTuple = [header, ...data.map(row => Object.values(row).map(String))]
-    return dataToTuple.map(row =>
-      row
-      .map(v => v.replaceAll('"', '""')) 
-      .map(v => `"${v}"`) 
-      .join(',') 
-    ).join('\r\n'); 
-  }
-  
-  export function downloadCSV(content: string): void {
-    const uriEncodedContents =
-      'data:text/csv;charset=utf-8,%EF%BB%BF' + encodeURIComponent(content)
-      const downloadLink = document.createElement('a')
-      downloadLink.href = uriEncodedContents
-      downloadLink.download = 'export.csv'
-      downloadLink.click()
-  }
-  
-  const getExportData =  (username: string) =>  supabaseClient
-  .from("csv_export")
-  .select("*")
-  .eq("username", username)
+export function downloadCSV(content: string): void {
+  const uriEncodedContents =
+    "data:text/csv;charset=utf-8,%EF%BB%BF" + encodeURIComponent(content);
+  const downloadLink = document.createElement("a");
+  downloadLink.href = uriEncodedContents;
+  downloadLink.download = "export.csv";
+  downloadLink.click();
+}
 
-  export const getExportCSVByUsername = async (username: string) => {
-    const { data } = await getExportData(username)
-    if (data) {
-        downloadCSV(toCSV(data))
-    }
+export const getExportCSVByUsername = async (username: string) => {
+  const { data } = await supabaseClient
+    .from("csv_export")
+    .select("*")
+    .eq("username", username)
+    .csv();
 
-  };
+  if (data) {
+    downloadCSV(data);
+  }
+};
