abc9fca3099c12036cf26fb79bdb38c13e4fd623
Ville Heikkilä <ville.heikkila@reaktor.fi>
2022-09-18 14:55:15 +0900

    Remove remix implementation

--- app/routes/logout.tsx
+++
@@ -1,6 +0,0 @@
-import type { LoaderFunction } from "@remix-run/node";
-import { authenticator } from "~/auth.server";
-
-export const loader: LoaderFunction = async ({ request }) => {
-  await authenticator.logout(request, { redirectTo: "/login" });
-};
