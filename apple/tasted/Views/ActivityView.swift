63e9223e2138f944310f94ede1f2ed500fd0bac9
Ville Heikkilä <mail@villeheikkila.com>
2022-10-13 00:37:31 +0300

    Move NavigationStack up

--- apple/tasted/Views/ActivityView.swift
+++ apple/tasted/Views/ActivityView.swift
@@ -13,27 +13,15 @@ struct ActivityView: View {
     @StateObject private var model = ActivityViewModel()
 
     var body: some View {
-        NavigationStack {
-            
-            ScrollView {
-                ForEach(model.checkIns, id: \.id) { checkIn in
-                    NavigationLink(value: checkIn) {
-                        CheckInCardView(checkIn: checkIn)
-                    }
+        ScrollView {
+            ForEach(model.checkIns, id: \.id) { checkIn in
+                NavigationLink(value: checkIn) {
+                    CheckInCardView(checkIn: checkIn)
                 }
-                
-                
-            }.task {
-                model.getActivityFeed()
-            }.navigationDestination(for: CheckInResponse.self) { checkIn in
-                CheckInPageView(checkIn: checkIn)
             }
-            .navigationDestination(for: ProfileResponse.self) { profile in
-                ProfileView(userId: profile.id )
-            }
-            .navigationDestination(for: ProductResponse.self) { product in
-                ProductPageView(product: product)
-            }
+
+        }.task {
+            model.getActivityFeed()
         }
     }
 }
@@ -47,7 +35,7 @@ extension ActivityView {
                 .rpc(fn: "fnc__get_activity_feed")
                 .select(columns: "id, rating, review, created_at, profiles (id, username, avatar_url), products (id, name, description, sub_brands (id, name, brands (id, name, companies (id, name))), subcategories (id, name, categories (id, name))), check_in_reactions (id, created_by, profiles (id, username, avatar_url))")
                 .limit(count: 5)
-            
+
             Task {
                 let checkIns = try await query.execute().decoded(to: [CheckInResponse].self)
                 DispatchQueue.main.async {
