97c121eadaac9f578b411d2dd4b7e2bf7330204a
Ville Heikkilä <mail@villeheikkila.com>
2022-10-22 02:08:40 +0300

    Enable image caching

--- apple/tastey/Helpers/Extensions.swift
+++ apple/tastey/Helpers/Extensions.swift
@@ -1,6 +1,13 @@
+import SwiftUI
+
 extension Array where Element: Equatable {
  mutating func remove(object: Element) {
      guard let index = firstIndex(of: object) else {return}
      remove(at: index)
  }
 }
+
+extension URLCache {
+    static let imageCache = URLCache(memoryCapacity: 512*1000*1000, diskCapacity: 10*1000*1000*1000)
+}
+
