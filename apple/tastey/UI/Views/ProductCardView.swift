afa50fcc14eefbba300144319b4e1504b786315c
Ville Heikkilä <mail@villeheikkila.com>
2022-10-24 00:34:58 +0300

    Add link to company page from product page

--- apple/tastey/UI/Views/ProductCardView.swift
+++ apple/tastey/UI/Views/ProductCardView.swift
@@ -6,9 +6,9 @@ struct ProductCardView: View {
     var body: some View {
         VStack {
             HStack {
-            if let categoryName = product.getCategory() {
-                Text(categoryName.rawValue.capitalized).font(.system(size: 12, weight: .bold, design: .default))
-            }
+                if let categoryName = product.getCategory() {
+                    Text(categoryName.rawValue.capitalized).font(.system(size: 12, weight: .bold, design: .default))
+                }
                 ForEach(product.subcategories, id: \.id) { subcategory in
                     ChipView(title: subcategory.name, cornerRadius: 5)
                 }
@@ -18,9 +18,11 @@ struct ProductCardView: View {
                 .font(.system(size: 18, weight: .bold, design: .default))
                 .foregroundColor(.primary)
             HStack {
-                Text(product.getDisplayName(.brandOwner))
-                    .font(.system(size: 16, weight: .bold, design: .default))
-                    .foregroundColor(.secondary)
+                NavigationLink(value: product.subBrand.brand.brandOwner) {
+                    Text(product.getDisplayName(.brandOwner))
+                        .font(.system(size: 16, weight: .bold, design: .default))
+                        .foregroundColor(.secondary)
+                }
             }
         }
         .padding(.all, 10)
