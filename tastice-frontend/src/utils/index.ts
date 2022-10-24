46fc67035de31ce509c2ffba9d694eadb7715447
Ville Heikkilä <mail@villeheikkila.com>
2022-10-24 19:13:33 +0300

    Add license

--- tastice-frontend/src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-export const parseToken = (data: any): string | undefined => {
-    try {
-        const result = JSON.parse(data);
-        return result.token;
-    } catch {
-        return undefined;
-    }
-};
