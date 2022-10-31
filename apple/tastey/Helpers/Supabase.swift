a21b17f93265dda6e53590d0072a3c562a918896
Ville Heikkilä <mail@villeheikkila.com>
2022-10-31 14:24:04 +0100

    Fix verification check

--- apple/tastey/Helpers/Supabase.swift
+++ apple/tastey/Helpers/Supabase.swift
@@ -2,7 +2,7 @@ import Foundation
 import Supabase
 
 class Supabase {
-    static var testing = false
+    static var testing = true
     static var testingSupabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs"
     static var testingUrlString = "http://localhost:54321"
     static var supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRta3Z1cW9vY3RvbHZoZHN1Ym90Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjUzODg1MTQsImV4cCI6MTk4MDk2NDUxNH0.KmVr-_SLfU86G5CUfaR6plT4wdgab1DHpOpFmLtJlu8"
