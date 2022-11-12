111cd76201f355aae35f9d692de7c4ac4ca3e279
Ville Heikkilä <mail@villeheikkila.com>
2022-11-12 16:42:57 +0200

    feat(apple): add missing entitlement, initial work on deep linking

--- apple/TasteNotes/UI/Components/NavigationView.swift
+++ apple/TasteNotes/UI/Components/NavigationView.swift
@@ -4,39 +4,45 @@ import SwiftUI
 struct NavigationStackView: View {
     @StateObject var navigator = Navigator()
 
-    
     var body: some View {
         NavigationStack(path: $navigator.path) {
             AddRoutesView {
                 TabbarView()
             }
             .navigationBarItems(leading:
-                                    NavigationLink(value: Route.currentUserFriends) {
-                Image(systemName: "person.2").imageScale(.large)
-                
-            },
-                                trailing: NavigationLink(value: Route.settings) {
-                Image(systemName: "gear").imageScale(.large)
-            })
-        }.environmentObject(navigator)
+                NavigationLink(value: Route.currentUserFriends) {
+                    Image(systemName: "person.2").imageScale(.large)
+
+                },
+                trailing: NavigationLink(value: Route.settings) {
+                    Image(systemName: "gear").imageScale(.large)
+                })
+        }
+        .environmentObject(navigator)
+        .onOpenURL { url in
+            guard let scheme = url.scheme, scheme == "tastenotes" else { return }
+            print(scheme)
+            guard let info = url.host else { return }
+            print(info)
+        }
     }
 }
 
 class Navigator: ObservableObject {
     @Published var path = NavigationPath()
-    
+
     func gotoHomePage() {
-        path.removeLast(path.count)
+        path = NavigationPath()
     }
-    
+
     func removeLast() {
         path.removeLast()
     }
-    
+
     func tapOnSecondPage() {
         path.removeLast()
     }
-    
+
     func navigateTo(destination: some Hashable, resetStack: Bool) {
         if resetStack {
             path.removeLast(path.count)
@@ -59,7 +65,7 @@ enum Route: Hashable {
 
 struct AddRoutesView<Content: View>: View {
     var content: () -> Content
-    
+
     var body: some View {
         content()
             .navigationDestination(for: CheckIn.self) { checkIn in
