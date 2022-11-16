88c719f3ad2937aa45b732b4a9262fea1ee65846
Ville Heikkilä <mail@villeheikkila.com>
2022-11-16 23:55:15 +0200

    feat(next): add app store banner

--- next/pages/_app.tsx
+++ next/pages/_app.tsx
@@ -21,6 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
         />
+        <meta name="apple-itunes-app" content="app-id=6443958544" />
       </Head>
       <App theme="ios" safeAreas>
         <Component {...pageProps} />
