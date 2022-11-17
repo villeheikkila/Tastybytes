878cd7ff642232d44b599a6d6735902fc1d6a31d
Ville Heikkilä <mail@villeheikkila.com>
2022-11-17 23:32:37 +0200

    fix(apple): remove navigation view from search page, move state up

--- apple/TasteNotes/UI/Screens/SearchScreenView.swift
+++ apple/TasteNotes/UI/Screens/SearchScreenView.swift
@@ -7,55 +7,51 @@ enum SearchScope: String, CaseIterable {
 }
 
 struct SearchScreenView: View {
-    @StateObject private var viewModel = ViewModel()
-    @Binding var showBarcodeScanner: Bool
+    @ObservedObject var viewModel: SearchScreenViewModel
 
     var body: some View {
-        NavigationView {
-            List {
-                switch viewModel.searchScope {
-                case .products:
-                    productResults
-                    if viewModel.isSearched {
-                        Section {
-                            NavigationLink("Add new", value: Route.addProduct)
-                                .fontWeight(.medium)
-                        } header: {
-                            Text("Didn't find a product you were looking for?")
-                        }
-                        .textCase(nil)
+        List {
+            switch viewModel.searchScope {
+            case .products:
+                productResults
+                if viewModel.isSearched {
+                    Section {
+                        NavigationLink("Add new", value: Route.addProduct)
+                            .fontWeight(.medium)
+                    } header: {
+                        Text("Didn't find a product you were looking for?")
                     }
-                case .companies:
-                    companyResults
-                case .users:
-                    profileResults
+                    .textCase(nil)
                 }
+            case .companies:
+                companyResults
+            case .users:
+                profileResults
             }
-            .listStyle(InsetGroupedListStyle())
-            .searchable(text: $viewModel.searchTerm)
-            .searchScopes($viewModel.searchScope) {
-                Text("Products").tag(SearchScope.products)
-                Text("Companies").tag(SearchScope.companies)
-                Text("Users").tag(SearchScope.users)
-            }
-            .onChange(of: viewModel.searchScope, perform: { _ in
-                viewModel.search()
-            })
-            .onChange(of: viewModel.searchTerm, perform: {
-                term in
-                if term.isEmpty {
-                    viewModel.resetSearch()
-                }
-            })
-            .onSubmit(of: .search, viewModel.search)
         }
-        .sheet(isPresented: $showBarcodeScanner) {
+        .listStyle(InsetGroupedListStyle())
+        .searchable(text: $viewModel.searchTerm)
+        .searchScopes($viewModel.searchScope) {
+            Text("Products").tag(SearchScope.products)
+            Text("Companies").tag(SearchScope.companies)
+            Text("Users").tag(SearchScope.users)
+        }
+        .onChange(of: viewModel.searchScope, perform: { _ in
+            viewModel.search()
+        })
+        .onChange(of: viewModel.searchTerm, perform: {
+            term in
+            if term.isEmpty {
+                viewModel.resetSearch()
+            }
+        })
+        .onSubmit(of: .search, viewModel.search)
+        .sheet(isPresented: $viewModel.showBarcodeScanner) {
             BarcodeScannerSheetView(onComplete: {
                 code in print(code)
             })
             .presentationDetents([.medium])
         }
-
     }
 
     var profileResults: some View {
@@ -91,74 +87,71 @@ struct SearchScreenView: View {
     }
 }
 
-extension SearchScreenView {
-    class ViewModel: ObservableObject {
-        @Published var searchTerm: String = ""
-        @Published var products = [ProductJoined]()
-        @Published var profiles = [Profile]()
-        @Published var companies = [Company]()
+class SearchScreenViewModel: ObservableObject {
+    @Published var searchTerm: String = ""
+    @Published var products = [ProductJoined]()
+    @Published var profiles = [Profile]()
+    @Published var companies = [Company]()
+    @Published var showBarcodeScanner = false
+    @Published var isSearched = false
+    @Published var searchScope: SearchScope = .products
 
-        @Published var isSearched = false
-        @Published var searchScope: SearchScope = .products
+    func resetSearch() {
+        profiles = []
+        products = []
+        companies = []
+    }
 
-        func resetSearch() {
-            profiles = []
-            products = []
-            companies = []
-        }
-
-        func searchProducts() {
-            Task {
-                switch await repository.product.search(searchTerm: searchTerm) {
-                case let .success(searchResults):
-                    await MainActor.run {
-                        self.products = searchResults
-                        self.isSearched = true
-                    }
-                case let .failure(error):
-                    print(error)
+    func searchProducts() {
+        Task {
+            switch await repository.product.search(searchTerm: searchTerm) {
+            case let .success(searchResults):
+                await MainActor.run {
+                    self.products = searchResults
+                    self.isSearched = true
                 }
+            case let .failure(error):
+                print(error)
             }
         }
+    }
 
-        func searchProfiles() {
-            let currentUserId = repository.auth.getCurrentUserId()
-            Task {
-                switch await repository.profile.search(searchTerm: searchTerm, currentUserId: currentUserId) {
-                case let .success(searchResults):
-                    await MainActor.run {
-                        self.profiles = searchResults
-                    }
-                case let .failure(error):
-                    print(error)
+    func searchProfiles() {
+        let currentUserId = repository.auth.getCurrentUserId()
+        Task {
+            switch await repository.profile.search(searchTerm: searchTerm, currentUserId: currentUserId) {
+            case let .success(searchResults):
+                await MainActor.run {
+                    self.profiles = searchResults
+                }
+            case let .failure(error):
+                print(error)
+            }
+        }
+    }
+
+    func searchCompanies() {
+        Task {
+            switch await repository.company.search(searchTerm: searchTerm) {
+            case let .success(searchResults):
+                await MainActor.run {
+                    self.companies = searchResults
                 }
 
+            case let .failure(error):
+                print(error)
             }
         }
+    }
 
-        func searchCompanies() {
-            Task {
-                switch await repository.company.search(searchTerm: searchTerm) {
-                case let .success(searchResults):
-                    await MainActor.run {
-                        self.companies = searchResults
-                    }
-
-                case let .failure(error):
-                    print(error)
-                }
-            }
-        }
-
-        func search() {
-            switch searchScope {
-            case .products:
-                searchProducts()
-            case .companies:
-                searchCompanies()
-            case .users:
-                searchProfiles()
-            }
+    func search() {
+        switch searchScope {
+        case .products:
+            searchProducts()
+        case .companies:
+            searchCompanies()
+        case .users:
+            searchProfiles()
         }
     }
 }
