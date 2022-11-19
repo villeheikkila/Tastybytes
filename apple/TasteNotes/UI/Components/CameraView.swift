fe228156923411b4cfb9f98f53e3a3ae30978146
Ville Heikkilä <mail@villeheikkila.com>
2022-11-19 12:39:01 +0200

    feat(apple): add support for takin check-in photos from camera

--- apple/TasteNotes/UI/Components/CameraView.swift
+++ apple/TasteNotes/UI/Components/CameraView.swift
@@ -5,6 +5,7 @@ import UIKit
 struct CameraView: UIViewControllerRepresentable {
     typealias UIViewControllerType = UIImagePickerController
     let onClose: () -> Void
+    let onCapture: (_ image: UIImage) -> Void
 
     func makeUIViewController(context: Context) -> UIViewControllerType {
         let viewController = UIViewControllerType()
@@ -17,7 +18,7 @@ struct CameraView: UIViewControllerRepresentable {
     }
 
     func makeCoordinator() -> CameraView.Coordinator {
-        return Coordinator(self, onClose: onClose)
+        return Coordinator(self, onClose: onClose, onCapture: onCapture)
     }
 }
 
@@ -25,10 +26,12 @@ extension CameraView {
     class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
         var parent: CameraView
         let onClose: () -> Void
+        let onCapture: (_ image: UIImage) -> Void
 
-        init(_ parent: CameraView, onClose: @escaping () -> Void) {
+        init(_ parent: CameraView, onClose: @escaping () -> Void, onCapture: @escaping (_ image: UIImage) -> Void) {
             self.parent = parent
             self.onClose = onClose
+            self.onCapture = onCapture
         }
 
         func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
@@ -37,7 +40,7 @@ extension CameraView {
 
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
             if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-                UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+                onCapture(image)
             }
         }
     }
