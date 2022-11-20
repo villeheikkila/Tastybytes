35d1a9b6aa18aced14aed0f42ec9b5b923e269e8
Ville Heikkilä <mail@villeheikkila.com>
2022-11-21 00:13:32 +0200

    feat(apple): improve companies page by adding friend stats, better styling

--- apple/TasteNotes/Models/Company.swift
+++ apple/TasteNotes/Models/Company.swift
@@ -104,13 +104,6 @@ extension CompanyJoined: Decodable {
     }
 }
 
-struct CompanySummary {
-    let totalCheckIns: Int
-    let averageRating: Double?
-    let currentUserTotalCheckIns: Int
-    let currentUserAverageRating: Double?
-}
-
 struct GetCompanySummaryParams: Encodable {
     let p_company_id: Int
     
@@ -119,10 +112,21 @@ struct GetCompanySummaryParams: Encodable {
     }
 }
 
+struct CompanySummary {
+    let totalCheckIns: Int
+    let averageRating: Double?
+    let friendsTotalCheckIns: Int
+    let friendsAverageRating: Double?
+    let currentUserTotalCheckIns: Int
+    let currentUserAverageRating: Double?
+}
+
 extension CompanySummary: Decodable {
     enum CodingKeys: String, CodingKey {
         case totalCheckIns = "total_check_ins"
         case averageRating = "average_rating"
+        case friendsTotalCheckIns = "friends_check_ins"
+        case friendsAverageRating = "friends_average_rating"
         case currentUserTotalCheckIns = "current_user_check_ins"
         case currentUserAverageRating = "current_user_average_rating"
     }
@@ -131,6 +135,8 @@ extension CompanySummary: Decodable {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         totalCheckIns = try values.decode(Int.self, forKey: .totalCheckIns)
         averageRating = try values.decodeIfPresent(Double.self, forKey: .averageRating)
+        friendsTotalCheckIns = try values.decode(Int.self, forKey: .friendsTotalCheckIns)
+        friendsAverageRating = try values.decodeIfPresent(Double.self, forKey: .friendsAverageRating)
         currentUserTotalCheckIns = try values.decode(Int.self, forKey: .currentUserTotalCheckIns)
         currentUserAverageRating = try values.decodeIfPresent(Double.self, forKey: .currentUserAverageRating)
     }
