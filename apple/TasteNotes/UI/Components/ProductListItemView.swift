112d1eb52a21e21e1ffb90354193bbd989a33081
Ville Heikkilä <mail@villeheikkila.com>
2022-11-21 19:56:05 +0200

    feat: move category under name in search view

--- apple/TasteNotes/UI/Components/ProductListItemView.swift
+++ apple/TasteNotes/UI/Components/ProductListItemView.swift
@@ -5,8 +5,7 @@ struct ProductListItemView: View {
     @State private var searchText = ""
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 5) {
-            Text(product.category.name.rawValue.capitalized).font(.system(size: 12, weight: .bold, design: .default))
+        VStack(alignment: .leading, spacing: 3) {
             Text(product.getDisplayName(.fullName))
                 .font(.system(size: 16, weight: .bold, design: .default))
                 .foregroundColor(.primary)
@@ -14,7 +13,9 @@ struct ProductListItemView: View {
             Text(product.getDisplayName(.brandOwner))
                 .font(.system(size: 14, weight: .bold, design: .default))
                 .foregroundColor(.secondary)
+            
             HStack {
+                Text(product.category.name.rawValue.capitalized).font(.system(size: 12, weight: .bold, design: .default))
                 ForEach(product.subcategories, id: \.id) { subcategory in
                     ChipView(title: subcategory.name, cornerRadius: 5)
                 }
