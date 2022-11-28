881131653797eb9bb1263975dfb77e83b45b5fbf
Ville Heikkilä <mail@villeheikkila.com>
2022-11-28 12:28:57 +0200

    feat(apple): redesign check-in view a bit, make it more confomant with Swift UI patterns

--- apple/TasteNotes/UI/Components/ServingStyleLabelView.swift
+++ apple/TasteNotes/UI/Components/ServingStyleLabelView.swift
@@ -1,10 +1,11 @@
 import SwiftUI
 
 struct ServingStyleLabelView: View {
-    let servingStyle: ServingStyle
+    let servingStyleName: ServingStyleName
+    
     var body: some View {
         HStack {
-            Text(servingStyle.name.rawValue.capitalized)
+            Text(servingStyleName.rawValue.capitalized)
                 .font(.system(size: 12, weight: .bold, design: .default))
         }
     }
@@ -12,6 +13,6 @@ struct ServingStyleLabelView: View {
 
 struct ServingStyleLabelView_Previews: PreviewProvider {
     static var previews: some View {
-        ServingStyleLabelView(servingStyle: ServingStyle(id: 0, name: ServingStyleName.bottle))
+        ServingStyleLabelView(servingStyleName: ServingStyleName.bottle)
     }
 }
