aeb3882afdcfcd2c3e6679413b4b8741b6f8d6e2
Ville Heikkilä <mail@villeheikkila.com>
2022-12-02 17:24:44 +0200

    feat(apple): minor refactoring

--- apple/TasteNotes/Repositories/LocationRepository.swift
+++ apple/TasteNotes/Repositories/LocationRepository.swift
@@ -9,12 +9,10 @@ struct SupabaseLocationRepository: LocationRepository {
     let client: SupabaseClient
 
     func insert(location: Location) async -> Result<Location, Error> {
-        let params = location.getNew()
-        print(params)
         do {
             let result =  try await client
                 .database
-                .rpc(fn: "fnc__get_location_insert_if_not_exist", params: params)
+                .rpc(fn: "fnc__get_location_insert_if_not_exist", params: location.getNew())
                 .select(columns: Location.getQuery(.joined(false)))
                 .single()
                 .execute()
