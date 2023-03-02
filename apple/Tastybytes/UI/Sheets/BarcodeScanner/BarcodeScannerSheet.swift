deb74655b5193583f72413a66c617d160fbe61a7
Ville Heikkilä <mail@villeheikkila.com>
2023-03-02 16:38:42 +0200

    feat: add toggle for barcode scanner torch

--- apple/Tastybytes/UI/Sheets/BarcodeScanner/BarcodeScannerSheet.swift
+++ apple/Tastybytes/UI/Sheets/BarcodeScanner/BarcodeScannerSheet.swift
@@ -5,6 +5,7 @@ struct BarcodeScannerSheet: View {
   @Environment(\.dismiss) private var dismiss
   @State private var showBarcodeTextField = false
   @State private var barcodeInput = ""
+  @State private var isTorchOn = false
 
   let onComplete: (_ barcode: Barcode) -> Void
 
@@ -23,24 +24,45 @@ struct BarcodeScannerSheet: View {
         }
 
       } else {
-        ScannerView(scanTypes: [.codabar, .code39, .ean8, .ean13]) { response in
+        ScannerView(scanTypes: [.codabar, .code39, .ean8, .ean13], completion: { response in
           if case let .success(result) = response {
             onComplete(result)
             dismiss()
           }
-        }
+        }, isTorchOn: isTorchOn)
       }
     }
     .navigationTitle("Barcode Scanner")
-    .navigationBarItems(leading: Button(role: .cancel, action: { dismiss() }, label: {
-      Text("Cancel").bold()
-    }), trailing: Button(action: { withAnimation { showBarcodeTextField.toggle() } }, label: {
-      Label(
-        showBarcodeTextField ? "Show scanner" : "Add barcode manually",
-        systemImage: showBarcodeTextField ? "barcode.viewfinder" : "character.cursor.ibeam"
-      )
-      .labelStyle(.iconOnly)
-      .imageScale(.large)
-    }))
+    .toolbar {
+      toolbarContent
+    }
+  }
+
+  @ToolbarContentBuilder
+  private var toolbarContent: some ToolbarContent {
+    ToolbarItemGroup(placement: .navigationBarLeading) {
+      Button(role: .cancel, action: { dismiss() }, label: {
+        Text("Cancel")
+          .bold()
+      })
+    }
+    ToolbarItemGroup(placement: .navigationBarTrailing) {
+      Button(action: { withAnimation { showBarcodeTextField.toggle() } }, label: {
+        Label(
+          showBarcodeTextField ? "Show scanner" : "Add barcode manually",
+          systemImage: showBarcodeTextField ? "barcode.viewfinder" : "character.cursor.ibeam"
+        )
+        .labelStyle(.iconOnly)
+        .imageScale(.large)
+      })
+      Button(action: { withAnimation { isTorchOn.toggle() } }, label: {
+        Label(
+          "Turn the torch \(isTorchOn ? "off" : "on")",
+          systemImage: isTorchOn ? "flashlight.on.fill" : "flashlight.off.fill"
+        )
+        .labelStyle(.iconOnly)
+        .imageScale(.large)
+      })
+    }
   }
 }
