2107c9a375d8cbf00954d020dabb7fe23d5de6af
Ville Heikkilä <mail@villeheikkila.com>
2023-06-07 20:02:41 +0300

    fix: use logo instead of icon for logo

--- apple/Tastybytes/UI/Components/AppLogoView.swift
+++ apple/Tastybytes/UI/Components/AppLogoView.swift
@@ -4,7 +4,7 @@ struct AppLogoView: View {
   let size = min(UIScreen.main.bounds.width / 4.5, 300)
 
   var body: some View {
-      Image(getCurrentAppIcon().icon)
+      Image(getCurrentAppIcon().logo)
       .resizable()
       .aspectRatio(contentMode: .fill)
       .frame(width: size, height: size)
