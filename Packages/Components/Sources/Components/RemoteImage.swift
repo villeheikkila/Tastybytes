0e25c3b5d72bd2809ff9dd16a74a1558ed7a8db5
Ville Heikkilä <mail@villeheikkila.com>
2023-12-03 17:35:10 +0200

    chore: update Nuke

--- Packages/Components/Sources/Components/RemoteImage.swift
+++ Packages/Components/Sources/Components/RemoteImage.swift
@@ -4,9 +4,8 @@ import SwiftUI
 public struct RemoteImage<Content: View>: View {
     let url: URL?
     let content: (LazyImageState) -> Content
-    public init(url: URL?,
-                @ViewBuilder content: @escaping (LazyImageState) -> Content)
-    {
+
+    public init(url: URL?, @ViewBuilder content: @escaping (LazyImageState) -> Content) {
         self.url = url
         self.content = content
     }
