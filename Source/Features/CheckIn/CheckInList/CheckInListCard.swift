45c3ea2c5347ae9b536d7eb4b9fedfeb8c54e7a4
Ville Heikkilä <mail@villeheikkila.com>
2024-01-16 21:30:47 +0200

    fix: sheets in check-in lists

--- Source/Features/CheckIn/CheckInList/CheckInListCard.swift
+++ Source/Features/CheckIn/CheckInList/CheckInListCard.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct CheckInListCard: View {
     @Environment(ProfileEnvironmentModel.self) private var profileEnvironmentModel
     @Environment(Router.self) private var router
+    @State private var sheet: Sheet?
     @State private var showDeleteCheckInConfirmationDialog = false
     @State private var showDeleteConfirmationFor: CheckIn? {
         didSet {
@@ -24,15 +25,74 @@ struct CheckInListCard: View {
 
     var body: some View {
         CheckInCard(checkIn: checkIn, loadedFrom: loadedFrom)
-            .checkInContextMenu(
-                router: router,
-                profileEnvironmentModel: profileEnvironmentModel,
-                checkIn: checkIn,
-                onCheckInUpdate: onUpdate,
-                onDelete: { checkIn in
-                    showDeleteConfirmationFor = checkIn
+            .contextMenu {
+                ControlGroup {
+                    CheckInShareLinkView(checkIn: checkIn)
+                    if checkIn.profile.id == profileEnvironmentModel.id {
+                        Button(
+                            "Edit",
+                            systemImage: "pencil",
+                            action: {
+                                sheet = .checkIn(checkIn, onUpdate: { updatedCheckIn in
+                                    onUpdate(updatedCheckIn)
+                                })
+                            }
+                        )
+                        Button(
+                            "Delete",
+                            systemImage: "trash.fill",
+                            role: .destructive,
+                            action: {
+                                showDeleteConfirmationFor = checkIn
+                            }
+                        )
+                    } else {
+                        Button(
+                            "Check-in",
+                            systemImage: "pencil",
+                            action: {
+                                sheet = .newCheckIn(checkIn.product, onCreation: { checkIn in
+                                    router.navigate(screen: .checkIn(checkIn))
+                                })
+                            }
+                        )
+                        ReportButton(entity: .checkIn(checkIn))
+                    }
                 }
-            )
+                Divider()
+                RouterLink("Open Product", systemImage: "grid", screen: .product(checkIn.product))
+                RouterLink(
+                    "Open Brand Owner",
+                    systemImage: "network",
+                    screen: .company(checkIn.product.subBrand.brand.brandOwner)
+                )
+                RouterLink(
+                    "Open Brand",
+                    systemImage: "cart",
+                    screen: .fetchBrand(checkIn.product.subBrand.brand)
+                )
+                RouterLink(
+                    "Open Sub-brand",
+                    systemImage: "cart",
+                    screen: .fetchSubBrand(checkIn.product.subBrand)
+                )
+                if let location = checkIn.location {
+                    RouterLink(
+                        "Open Location",
+                        systemImage: "network",
+                        screen: .location(location)
+                    )
+                }
+                if let purchaseLocation = checkIn.purchaseLocation {
+                    RouterLink(
+                        "Open Purchase Location",
+                        systemImage: "network",
+                        screen: .location(purchaseLocation)
+                    )
+                }
+                Divider()
+            }
+            .sheets(item: $sheet)
             .confirmationDialog(
                 "check-in.delete-confirmation.title",
                 isPresented: $showDeleteCheckInConfirmationDialog,
