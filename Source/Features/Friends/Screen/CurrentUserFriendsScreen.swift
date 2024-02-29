5e87414ec542e3b563cef872e7a4db4de8721bab
Ville Heikkilä <mail@villeheikkila.com>
2024-02-29 20:13:30 +0200

    refactor: clean up current user friend screen

---
+++ Source/Features/Friends/Screen/CurrentUserFriendsScreen.swift
@@ -0,0 +1,188 @@
+import Components
+import EnvironmentModels
+import Models
+import SwiftUI
+
+@MainActor
+struct CurrentUserFriendsScreen: View {
+    @Environment(ProfileEnvironmentModel.self) private var profileEnvironmentModel
+    @Environment(FriendEnvironmentModel.self) private var friendEnvironmentModel
+    @Environment(FeedbackEnvironmentModel.self) private var feedbackEnvironmentModel
+    @Environment(NotificationEnvironmentModel.self) private var notificationEnvironmentModel
+    @State private var friendToBeRemoved: Friend?
+    @State private var showUserSearchSheet = false
+    @State private var searchTerm = ""
+    @State private var sheet: Sheet?
+
+    var filteredFriends: [Friend] {
+        friendEnvironmentModel.acceptedOrPendingFriends.filter { friend in
+            searchTerm.isEmpty
+                || friend.getFriend(userId: profileEnvironmentModel.id).preferredName.lowercased()
+                .contains(searchTerm.lowercased())
+        }
+    }
+
+    var body: some View {
+        List(filteredFriends) { friend in
+            CurrentUserFriendListRow(friend: friend)
+        }
+        .listStyle(.insetGrouped)
+        .sensoryFeedback(.success, trigger: friendEnvironmentModel.isRefreshing) { oldValue, newValue in
+            oldValue && !newValue
+        }
+        .overlay {
+            if friendEnvironmentModel.friends.isEmpty {
+                ContentUnavailableView {
+                    Label("friends.contentUnavailable.noFriends", systemImage: "person.3")
+                }
+            } else if !searchTerm.isEmpty, filteredFriends.isEmpty {
+                ContentUnavailableView.search(text: searchTerm)
+            }
+        }
+        .navigationTitle("friends.title \(friendEnvironmentModel.friends.count.formatted())")
+        .navigationBarTitleDisplayMode(.inline)
+        .searchable(text: $searchTerm, placement: .navigationBarDrawer(displayMode: .always))
+        .refreshable {
+            await friendEnvironmentModel.refresh(withHaptics: true)
+        }
+        .initialTask {
+            await friendEnvironmentModel.refresh()
+        }
+        .task {
+            await notificationEnvironmentModel.markAllFriendRequestsAsRead()
+        }
+        .toolbar {
+            toolbarContent
+        }
+        .sheets(item: $sheet)
+    }
+
+    @ToolbarContentBuilder private var toolbarContent: some ToolbarContent {
+        ToolbarItemGroup(placement: .topBarTrailing) {
+            Button(
+                "friends.toolbar.showNameTag",
+                systemImage: "qrcode",
+                action: { sheet = .nameTag(onSuccess: { profileId in
+                    Task {
+                        await friendEnvironmentModel.sendFriendRequest(receiver: profileId)
+                    }
+                }) }
+            )
+            .labelStyle(.iconOnly)
+            .imageScale(.large)
+            .popoverTip(NameTagTip())
+
+            Button(
+                "friends.add.label", systemImage: "plus",
+                action: { sheet = .userSheet(
+                    mode: .add,
+                    onSubmit: {
+                        feedbackEnvironmentModel.toggle(.success("friends.add.success"))
+                    }
+                ) }
+            )
+            .labelStyle(.iconOnly)
+            .imageScale(.large)
+        }
+    }
+}
+
+@MainActor
+struct CurrentUserFriendListRow: View {
+    @Environment(ProfileEnvironmentModel.self) private var profileEnvironmentModel
+    @Environment(FriendEnvironmentModel.self) private var friendEnvironmentModel
+    @State private var showFriendDeleteConfirmation = false
+
+    let friend: Friend
+    
+    var body: some View {
+        FriendListItemView(profile: friend.getFriend(userId: profileEnvironmentModel.profile.id)) {
+            HStack {
+                if friend.status == .pending {
+                    Text(friend.status.label)
+                        .font(.footnote)
+                        .foregroundColor(.primary)
+                }
+                Spacer()
+                if friend.isPending(userId: profileEnvironmentModel.profile.id) {
+                    HStack(alignment: .center) {
+                        Label("friends.action.removeFriendRequest.label", systemImage: "person.fill.xmark")
+                            .imageScale(.large)
+                            .labelStyle(.iconOnly)
+                            .accessibilityAddTraits(.isButton)
+                            .onTapGesture {
+                                showFriendDeleteConfirmation = true
+                            }
+                        Label("friends.acceptRequest.label", systemImage: "person.badge.plus")
+                            .imageScale(.large)
+                            .labelStyle(.iconOnly)
+                            .accessibilityAddTraits(.isButton)
+                            .onTapGesture {
+                                Task {
+                                    await friendEnvironmentModel.updateFriendRequest(
+                                        friend: friend,
+                                        newStatus: .accepted
+                                    )
+                                }
+                            }
+                    }
+                }
+            }
+        }
+        .swipeActions {
+            Group {
+                if friend.isPending(userId: profileEnvironmentModel.profile.id) {
+                    ProgressButton(
+                        "friends.acceptRequest.label",
+                        systemImage: "person.badge.plus",
+                        action: {
+                            await friendEnvironmentModel.updateFriendRequest(
+                                friend: friend,
+                                newStatus: .accepted
+                            )
+                        }
+                    )
+                    .tint(.green)
+                }
+                deleteFriendButton
+                blockFriendButton
+            }.imageScale(.large)
+        }
+        .contextMenu {
+            deleteFriendButton
+            blockFriendButton
+        }
+        .confirmationDialog("friend.delete.confirmation.title",
+                            isPresented: $showFriendDeleteConfirmation,
+                            titleVisibility: .visible,
+                            presenting: friend)
+        { presenting in
+            ProgressButton(
+                "friend.delete.confirmation.label \(presenting.getFriend(userId: profileEnvironmentModel.id).preferredName)",
+                role: .destructive,
+                action: {
+                    await friendEnvironmentModel.removeFriendRequest(presenting)
+                }
+            )
+        }
+    }
+    
+    var deleteFriendButton: some View {
+        Button(
+            "labels.delete",
+            systemImage: "person.fill.xmark",
+            role: .destructive,
+            action: { showFriendDeleteConfirmation = true }
+        )
+    }
+    
+    var blockFriendButton: some View {
+        ProgressButton(
+            "friends.block.label",
+            systemImage: "person.2.slash",
+            action: {
+                await friendEnvironmentModel.updateFriendRequest(friend: friend, newStatus: .blocked)
+            }
+        )
+    }
+}
