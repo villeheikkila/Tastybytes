fe671a8e0b0209f9d09ec83e6e7e74c98cd70095
Ville Heikkilä <mail@villeheikkila.com>
2024-03-07 18:01:36 +0200

    wip: image popover and sharelink

--- Source/Features/CheckIn/CheckInCard/CheckInCardImage.swift
+++ Source/Features/CheckIn/CheckInCard/CheckInCardImage.swift
@@ -26,15 +26,7 @@ struct CheckInCardImage: View {
                         }
                         .accessibility(addTraits: .isButton)
                         .popover(isPresented: $showFullPicture) {
-                            RemoteImage(url: imageUrl) { state in
-                                if let image = state.image {
-                                    image
-                                        .resizable()
-                                        .scaledToFill()
-                                } else {
-                                    ProgressView()
-                                }
-                            }
+                            CheckInCardImagePopover(checkIn: checkIn, imageUrl: imageUrl)
                         }
                 } else {
                     BlurHashPlaceholder(blurHash: checkIn.images.first?.blurHash, height: 200)
@@ -42,6 +34,96 @@ struct CheckInCardImage: View {
             }
             .frame(height: 200)
             .padding(.vertical, 4)
+        }
+    }
+}
+
+struct CheckInCardImagePopover: View {
+    @Environment(\.dismiss) private var dismiss
+    let checkIn: CheckIn
+    let imageUrl: URL
+
+    var body: some View {
+        RemoteImage(url: imageUrl) { state in
+            if let image = state.image {
+                image
+                    .resizable()
+                    .scaledToFill()
+            } else {
+                ProgressView()
+            }
+        }
+        .ignoresSafeArea(.all)
+        .safeAreaInset(edge: .bottom, content: {
+            HStack(alignment: .top) {
+                HStack(alignment: .center) {
+                    Avatar(profile: checkIn.profile)
+                        .avatarSize(.large)
+                    VStack(alignment: .leading) {
+                        Text(checkIn.profile.preferredName)
+                            .font(.caption).bold()
+                            .foregroundColor(.primary)
+                        if let location = checkIn.location {
+                            Text(location.formatted(.withEmoji))
+                                .font(.caption).bold()
+                                .foregroundColor(.primary)
+                                .contentShape(Rectangle())
+                        }
+                    }
+                    Spacer()
+                    if let checkInAt = checkIn.checkInAt {
+                        Text(checkInAt.formatted(.customRelativetime))
+                            .font(.caption)
+                            .bold()
+                    } else {
+                        Text("checkIn.legacy.label")
+                            .font(.caption)
+                            .bold()
+                    }
+                }
+            }
+            .padding()
+            .background(.ultraThinMaterial)
+        })
+        .safeAreaInset(edge: .top, content: {
+            HStack(alignment: .center) {
+                Spacer()
+                CloseButton {
+                    dismiss()
+                }
+            }
+            .padding()
+            .background(.ultraThinMaterial)
+        })
+    }
+}
+
+struct ImageShareLink: View {
+    let url: URL
+
+    var transferable: ImageTransferable {
+        ImageTransferable(url: url)
+    }
+
+    var body: some View {
+        ShareLink(item: transferable, preview: .init("img", image: transferable))
+    }
+}
+
+struct ImageTransferable: Codable, Transferable {
+    let url: URL
+
+    func fetchImageData() async -> Data {
+        do {
+            return try await URLSession.shared.data(from: url).0
+        } catch {
+            return Data()
+        }
+    }
+
+    static var transferRepresentation: some TransferRepresentation {
+        DataRepresentation(exportedContentType: .jpeg) { transferable in
+            await transferable.fetchImageData()
         }
     }
 }
