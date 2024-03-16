1313a6f7dd7c51dfafcd9ff30f52fe8bb5c7bc67
Ville Heikkilä <mail@villeheikkila.com>
2024-03-16 21:11:32 +0200

    feat: make image viewer dragable, zoomable and improve look & feel

--- Source/Features/CheckIn/Sheet/CheckInImageSheet.swift
+++ Source/Features/CheckIn/Sheet/CheckInImageSheet.swift
@@ -6,31 +6,12 @@ import SwiftUI
 @MainActor
 struct CheckInImageSheet: View {
     @Environment(\.dismiss) private var dismiss
-    @State private var scale: CGFloat = 1.0
     let checkIn: CheckIn
     let imageUrl: URL
 
-    private let minScaleFactor = 0.8
-
     var body: some View {
         VStack(alignment: .center) {
-            RemoteImage(url: imageUrl) { state in
-                if let image = state.image {
-                    image
-                        .resizable()
-                        .scaledToFill()
-                        .scaleEffect(scale)
-                        .gesture(
-                            MagnificationGesture()
-                                .onChanged { scaleFactor in
-                                    guard scaleFactor > minScaleFactor else { return }
-                                    scale = scaleFactor.magnitude
-                                }
-                        )
-                } else {
-                    ProgressView()
-                }
-            }
+            ControllableImage(imageUrl: imageUrl)
         }
         .safeAreaInset(edge: .bottom, content: {
             HStack(alignment: .top) {
@@ -69,5 +50,56 @@ struct CheckInImageSheet: View {
                 ImageShareLink(url: imageUrl, title: "checkIn.shareLink.title \(checkIn.profile.preferredName) \(checkIn.product.formatted(.fullName))")
             }
         }
+    }
+}
+
+@MainActor
+struct ControllableImage: View {
+    @State private var scale: CGFloat = 1.0
+    @State private var location: CGPoint?
+    let imageUrl: URL
+
+    private let minScaleFactor = 0.8
+    
+    var dragGesture: some Gesture{
+        DragGesture()
+            .onChanged { value in
+                guard scale != 1.0 else { return }
+                withAnimation(.spring(response: 0.2, dampingFraction: 0.6)) {
+                    self.location = value.location
+                }
+            }
+    }
+    
+    var zoomGesture: some Gesture{
+        MagnificationGesture()
+            .onChanged { scaleFactor in
+                guard scaleFactor > minScaleFactor else { return }
+                scale = scaleFactor.magnitude
+            }
+    }
+
+    var body: some View {
+            GeometryReader { geometry in
+                RemoteImage(url: imageUrl) { state in
+                    if let image = state.image {
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .cornerRadius(8)
+                            .scaleEffect(scale)
+                            .position(location ?? .init(x: geometry.size.width / 2, y: geometry.size.height / 2))
+                            .simultaneousGesture(zoomGesture)
+                            .simultaneousGesture(dragGesture)
+                            .frame(size: .init(width: geometry.size.width * 0.8, height: geometry.size.height * 0.8))
+                            .onTapGesture(count: 2) {
+                                scale = 1
+                                location = .init(x: geometry.size.width / 2, y: geometry.size.height / 2)
+                            }
+                    } else {
+                        ProgressView()
+                    }
+                }
+            }
     }
 }
