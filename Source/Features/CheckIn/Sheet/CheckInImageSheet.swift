7db133f74ff6fe605ec15dd32a80e257a93a91b2
Ville Heikkilä <mail@villeheikkila.com>
2024-03-16 21:20:02 +0200

    feat: improve check-in image sheet check-in section

--- Source/Features/CheckIn/Sheet/CheckInImageSheet.swift
+++ Source/Features/CheckIn/Sheet/CheckInImageSheet.swift
@@ -14,35 +14,7 @@ struct CheckInImageSheet: View {
             ControllableImage(imageUrl: imageUrl)
         }
         .safeAreaInset(edge: .bottom, content: {
-            HStack(alignment: .top) {
-                HStack(alignment: .center) {
-                    Avatar(profile: checkIn.profile)
-                        .avatarSize(.large)
-                    VStack(alignment: .leading) {
-                        Text(checkIn.profile.preferredName)
-                            .font(.caption).bold()
-                            .foregroundColor(.primary)
-                        if let location = checkIn.location {
-                            Text(location.formatted(.withEmoji))
-                                .font(.caption).bold()
-                                .foregroundColor(.primary)
-                                .contentShape(Rectangle())
-                        }
-                    }
-                    Spacer()
-                    if let checkInAt = checkIn.checkInAt {
-                        Text(checkInAt.formatted(.customRelativetime))
-                            .font(.caption)
-                            .bold()
-                    } else {
-                        Text("checkIn.legacy.label")
-                            .font(.caption)
-                            .bold()
-                    }
-                }
-            }
-            .padding()
-            .background(.ultraThinMaterial)
+            CheckInImageCheckInSection(checkIn: checkIn)
         })
         .toolbar {
             ToolbarDismissAction()
@@ -54,24 +26,50 @@ struct CheckInImageSheet: View {
 }
 
 @MainActor
+struct CheckInImageCheckInSection: View {
+    let checkIn: CheckIn
+
+    var body: some View {
+        VStack {
+            CheckInCardHeader(
+                profile: checkIn.profile,
+                loadedFrom: .checkIn,
+                location: checkIn.location
+            )
+            CheckInCardProduct(
+                product: checkIn.product,
+                loadedFrom: .checkIn,
+                productVariant: checkIn.variant,
+                servingStyle: checkIn.servingStyle
+            )
+            CheckInCardCheckIn(checkIn: checkIn, loadedFrom: .checkIn)
+        }
+        .allowsHitTesting(false)
+        .padding()
+        .background(.ultraThinMaterial)
+    }
+}
+
+
+@MainActor
 struct ControllableImage: View {
     @State private var scale: CGFloat = 1.0
     @State private var location: CGPoint?
     let imageUrl: URL
 
     private let minScaleFactor = 0.8
-    
-    var dragGesture: some Gesture{
+
+    var dragGesture: some Gesture {
         DragGesture()
             .onChanged { value in
                 guard scale != 1.0 else { return }
                 withAnimation(.spring(response: 0.2, dampingFraction: 0.6)) {
-                    self.location = value.location
+                    location = value.location
                 }
             }
     }
-    
-    var zoomGesture: some Gesture{
+
+    var zoomGesture: some Gesture {
         MagnificationGesture()
             .onChanged { scaleFactor in
                 guard scaleFactor > minScaleFactor else { return }
@@ -80,26 +78,26 @@ struct ControllableImage: View {
     }
 
     var body: some View {
-            GeometryReader { geometry in
-                RemoteImage(url: imageUrl) { state in
-                    if let image = state.image {
-                        image
-                            .resizable()
-                            .scaledToFill()
-                            .cornerRadius(8)
-                            .scaleEffect(scale)
-                            .position(location ?? .init(x: geometry.size.width / 2, y: geometry.size.height / 2))
-                            .simultaneousGesture(zoomGesture)
-                            .simultaneousGesture(dragGesture)
-                            .frame(size: .init(width: geometry.size.width * 0.8, height: geometry.size.height * 0.8))
-                            .onTapGesture(count: 2) {
-                                scale = 1
-                                location = .init(x: geometry.size.width / 2, y: geometry.size.height / 2)
-                            }
-                    } else {
-                        ProgressView()
-                    }
+        GeometryReader { geometry in
+            RemoteImage(url: imageUrl) { state in
+                if let image = state.image {
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .cornerRadius(8)
+                        .scaleEffect(scale)
+                        .position(location ?? .init(x: geometry.size.width / 2, y: geometry.size.height / 2))
+                        .simultaneousGesture(zoomGesture)
+                        .simultaneousGesture(dragGesture)
+                        .frame(size: .init(width: geometry.size.width * 0.8, height: geometry.size.height * 0.8))
+                        .onTapGesture(count: 2) {
+                            scale = 1
+                            location = .init(x: geometry.size.width / 2, y: geometry.size.height / 2)
+                        }
+                } else {
+                    ProgressView()
                 }
             }
+        }
     }
 }
