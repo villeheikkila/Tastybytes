7777a3fa492b31978654d332c7a5f71d6a67edaf
Ville Heikkilä <mail@villeheikkila.com>
2024-05-11 10:13:49 +0300

    chore: update supabase-swift

--- Packages/Repositories/Package.swift
+++ Packages/Repositories/Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/supabase-community/supabase-swift.git",
-            from: "2.8.1"
+            from: "2.9.0"
         ),
         .package(name: "Models", path: "../Models"),
         .package(name: "Extensions", path: "../Extensions"),
