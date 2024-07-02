9a6daa3718def9b56cab0de45b2ff1f403875c00
Ville Heikkilä <mail@villeheikkila.com>
2024-07-02 23:57:13 +0300

    feat: group admin links

--- Source/Features/Admin/AdminScreen.swift
+++ Source/Features/Admin/AdminScreen.swift
@@ -4,12 +4,18 @@ import SwiftUI
 struct AdminScreen: View {
     var body: some View {
         List {
-            RouterLink("admin.category.title", systemImage: "rectangle.stack", open: .screen(.categoryManagement))
-            RouterLink("flavor.navigationTitle", systemImage: "face.smiling", open: .screen(.flavorManagement))
-            RouterLink("admin.verification.title", systemImage: "checkmark.seal", open: .screen(.verification))
-            RouterLink("admin.duplicates.title", systemImage: "plus.square.on.square", open: .screen(.duplicateProducts))
-            RouterLink("report.admin.navigationTitle", systemImage: "exclamationmark.bubble", open: .screen(.reports))
-            RouterLink("admin.locations.title", systemImage: "mappin.square", open: .screen(.locationManagement))
+            Section("admin.section.data.title") {
+                RouterLink("admin.category.title", systemImage: "rectangle.stack", open: .screen(.categoryManagement))
+                RouterLink("flavor.navigationTitle", systemImage: "face.smiling", open: .screen(.flavorManagement))
+            }
+            Section("admin.section.reports.title") {
+                RouterLink("admin.duplicates.title", systemImage: "plus.square.on.square", open: .screen(.duplicateProducts))
+                RouterLink("report.admin.navigationTitle", systemImage: "exclamationmark.bubble", open: .screen(.reports))
+            }
+            Section("admin.section.management.title") {
+                RouterLink("admin.verification.title", systemImage: "checkmark.seal", open: .screen(.verification))
+                RouterLink("admin.locations.title", systemImage: "mappin.square", open: .screen(.locationManagement))
+            }
         }
         .listStyle(.insetGrouped)
         .navigationBarTitle("admin.navigationTitle")
