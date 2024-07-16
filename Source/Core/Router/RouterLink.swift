bfe3eb376b14d1fe99fe632cee077c5efa008802
Ville Heikkilä <mail@villeheikkila.com>
2024-07-16 21:56:08 +0300

    fix: disable use of navigation links due to double navigation that seems to be related to beta 3

--- Source/Core/Router/RouterLink.swift
+++ Source/Core/Router/RouterLink.swift
@@ -21,7 +21,8 @@ struct RouterLink<LabelView: View>: View {
 
     @ViewBuilder
     private var content: some View {
-        if routerLinkMode == .preferNavigationLink, case let .screen(screen, resetStack, removeLast) = open, !resetStack || !removeLast {
+        // TODO: Enable after double navigation is fixed
+        if false, routerLinkMode == .preferNavigationLink, case let .screen(screen, resetStack, removeLast) = open, !resetStack || !removeLast {
             NavigationLink(value: screen, label: label)
         } else {
             Button(action: { router.open(open) }, label: label)
